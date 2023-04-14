06516bd822f9e61191a8c1e7bed872360111e3c8
Masechka <kleyzermp@gmail.com>
2023-04-14 21:53:36 +0300

    start working on viewing records

--- main.py
+++ main.py
@@ -26,6 +26,10 @@ class RecordStatesGroup(StatesGroup):
     places = State()
 
 
+class ViewingStatesGroup(StatesGroup):
+    ask_date = State()
+
+
 @dp.message_handler(commands='start')  # по команде /start выводиться вопрос + выбор кнопок
 async def start(message: types.Message):
     kb = [[types.KeyboardButton(text="Просмотр записей"), types.KeyboardButton(text="Создать запись")],
@@ -44,21 +48,6 @@ async def help(message: types.Message):
            '/new_day - создаст новую запись\nЕсли хочешь на что-то неотвечать напиши просто Пропустить!\n...'
     await message.answer(text)
 
-
-# @dp.message_handler(commands=['new_day'] or types.Message.text == 'Создать запись')  # создание записи для нового дня
-# async def new_day(message: types.Message):
-#     global flag
-#     kb = [
-#         [types.KeyboardButton(text="кнопка 1"), types.KeyboardButton(text="кнопка 2")],
-#     ]
-#     keyboard = types.ReplyKeyboardMarkup(
-#         keyboard=kb,
-#         resize_keyboard=True,
-#         input_field_placeholder="Напишите что-нибудь или нажмите на кнопку!"
-#     )
-#     text = 'Давайте создадим новую заметку!'
-#     flag = True
-#     await message.answer(text, reply_markup=keyboard)
 
 @dp.message_handler(commands=['new_day'] or types.Message.text == 'Создать запись')  # создание записи для нового дня
 async def new_day(message: types.Message) -> None:
@@ -227,6 +216,44 @@ async def input_places(message: types.Message, state: FSMContext) -> None:
         await state.finish()  # работа с состояниями завершена
 
 
+@dp.message_handler(commands=['viewing'] or types.Message.text == 'Просмотр записей')  # создание записи для нового дня
+async def viewing(message: types.Message) -> None:
+    dates = [] # здесь нужен список дат которые есть в БД
+    dates_str = ''
+    for date in dates:
+        dates_str += date + '\n'
+    if dates:
+        text = f'Есть записи на такие даты:\n{dates_str}'
+        await message.reply(text)
+        await ViewingStatesGroup.ask_date.set()  # устанавливается состояние ожидания для получения даты
+    else:
+        await message.reply('Пока нет ни одной записи')
+
+
+@dp.message_handler(state=ViewingStatesGroup.ask_date)
+async def ask_date(message: types.Message, state: FSMContext) -> None:
+    date = message.text
+    try:
+        res = bool(datetime.strptime(date, "%d-%m-%Y"))
+    except ValueError:
+        res = False
+    if res:
+        if datetime.strptime(str(dt.date.today()), "%Y-%m-%d") < datetime.strptime(date, "%d-%m-%Y"):
+            res = False
+    if res:
+        проверка_на_то_что_дата_есть_в_бд = False
+        # в if  надо написать проверку есть ли дата в БД
+        if проверка_на_то_что_дата_есть_в_бд:
+            text = '' # в эту переменную записать то, что есть в БД на эту дату в разделе текст
+            voice = '' # в эту переменную записать то, что есть в БД на эту дату в разделе голосовое сообщение
+            photo = '' # в эту переменную записать то, что есть в БД на эту дату в разделе фото
+            emoji = '' # в эту переменную записать то, что есть в БД на эту дату в разделе эмодзи
+            place = '' # в эту переменную записать то, что есть в БД на эту дату в разделе места
+        else:
+            await message.reply('На эту дату нет записи. Попробуй другую')
+    else:
+        await message.reply('Введена некорректная дата\nФормат даты ДД-ММ-ГГГГ')
+
 @dp.message_handler()
 async def first(message: types.Message, state: FSMContext):
     global flag
@@ -235,7 +262,7 @@ async def first(message: types.Message, state: FSMContext):
     elif message.text == 'Создать запись':
         await new_day(message)
     elif message.text == 'Просмотр записей':
-        pass
+        await viewing(message)
 
 
 if __name__ == '__main__':
